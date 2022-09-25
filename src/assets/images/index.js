e7c21a7cc50ba3dcac9ffd1f6992e5c71b71e960
markanthonyfamarin <markanthonyfamarin@gmail.com>
2022-09-25 21:48:51 +0800

    third commit

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,6 +3,10 @@ import image2 from "./image2.png";
 import menu from "./menu.png";
 import profile from "./profile.png";
 import arrowDown from "./arrowDown.png"
+import food1 from "./food1.png";
+import food2 from "./food2.png";
+import food3 from "./food3.png";
+import food4 from "./food4.png";
 
 
-export {image1, image2, menu, profile, arrowDown};
+export {image1, image2, menu, profile, arrowDown, food1, food2, food3, food4};
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,34 @@
 import HeaderNav2 from "../../components/headerNav2/HeaderNav2";
+import {food1, food2, food3, food4} from "../../assets/images";
 import "./styles.scss"
+
+const imageArr = [
+    {image: food1, name: "pizza"},
+    {image: food2, name: "burger"},
+    {image: food3, name: "sausage"},
+    {image: food4, name: "samosa"},
+];
 
 const Home = () => {
     return (
         <div className="home">
             <HeaderNav2/>
+            <h1 className="title">Enjoy Delicious food</h1>
+            <div className="foodContainer">
+                <div className="foodSlider">
+                    {
+                        imageArr.map(item=> (
+                            <section key={item.name} className="food">
+                                <span className="foodCont">
+                                    <img className="foodImage" src={item.image} alt="" />
+                                    <span className="foodName">{item.name}</span>
+                                </span>
+                            </section>
+
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     )
 }
