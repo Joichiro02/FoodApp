8ddbb703baeea01c7a4fee0647784dfb8e9fe977
markanthonyfamarin <markanthonyfamarin@gmail.com>
2022-09-27 05:09:27 +0800

    create card

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -7,6 +7,8 @@ import food1 from "./food1.png";
 import food2 from "./food2.png";
 import food3 from "./food3.png";
 import food4 from "./food4.png";
+import burger from "./burger.png";
+import star from "./star.png";
+import heart from "./heart.png";
 
-
-export {image1, image2, menu, profile, arrowDown, food1, food2, food3, food4};
+export {image1, image2, menu, profile, arrowDown, food1, food2, food3, food4, burger, star, heart};
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
 import HeaderNav2 from "../../components/headerNav2/HeaderNav2";
-import {food1, food2, food3, food4} from "../../assets/images";
+import {food1, food2, food3, food4, burger, star, heart} from "../../assets/images";
 import "./styles.scss"
 
 const imageArr = [
@@ -9,12 +9,39 @@ const imageArr = [
     {image: food4, name: "samosa"},
 ];
 
+const listArr = [
+    {
+        image: burger,
+        name: "Big cheese burger",
+        description: "No 10 opp lekki phase 1 bridge in sangotedo estate",
+        rate: 4,
+        star: star,
+        heart: heart
+    },
+    {
+        image: burger,
+        name: "Big cheese burger",
+        description: "No 10 opp lekki phase 1 bridge in sangotedo estate",
+        rate: 4,
+        star: star,
+        heart: heart
+    },
+    {
+        image: burger,
+        name: "Big cheese burger",
+        description: "No 10 opp lekki phase 1 bridge in sangotedo estate",
+        rate: 4,
+        star: star,
+        heart: heart
+    }
+]
+
 const Home = () => {
     return (
         <div className="home">
             <HeaderNav2/>
             <h1 className="title">Enjoy Delicious food</h1>
-            <div className="foodContainer">
+            <div className="foodCategoryContainer">
                 <div className="foodSlider">
                     {
                         imageArr.map(item=> (
@@ -29,6 +56,31 @@ const Home = () => {
                     }
                 </div>
             </div>
+            <div className="foodListContainer">
+                <header className="header">
+                    <h4 className="leftText">Popular restaurants</h4>
+                    <h6 className="rightText">View all(29)</h6>
+                </header>
+                <div className="foodItemContainer">
+                    <section className="foodItemBody">
+                        {listArr.map((food, index) => (
+                            <article className="foodItem" key={index}>
+                                <img className="foodImage" src={food.image} alt="" />
+                                <h4 className="foodTitle">{food.name}</h4>
+                                <h6 className="foodSubtitle">{food.description}</h6>
+                                <div className="foodRatingBody">
+                                    <span className="leftSide">
+                                        <img className="starIcon" src={food.star} alt="" />
+                                        <span className="starCount">{food.rate}+</span>
+                                    </span>
+                                    <img className="heartIcon" src={food.heart} alt="" />
+                                </div>
+                            </article>
+                        ))}
+                    </section>
+                </div>
+            </div>
+            <footer className="footerNav"></footer>
         </div>
     )
 }
