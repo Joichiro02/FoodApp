e3537fd4186249fe10bc539a3614432c1a39ac6f
markanthonyfamarin <markanthonyfamarin@gmail.com>
2022-09-29 04:50:10 +0800

    add layout in detail page

--- src/pages/details/Details.tsx
+++ src/pages/details/Details.tsx
@@ -9,6 +9,27 @@ const Details = () => {
             <div className="imageContent">
                 <img src={burger} alt="" />
             </div>
+            <section className="countContent">
+                <span className="minusBtn">-</span>
+                <span className="foodCount">2</span>
+                <span className="plusBtn">+</span>
+            </section>
+            <section className="titleContent">
+                <h2 className="title">Big boys Cheese burger</h2>
+                <div className="subtitleContent">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </section>
+            <section className="descriptionContent">
+                Our simple, classic cheeseburger begins with a 100% pure beef burger seasoned with just a pinch of salt and pepper. The McDonald’s Cheeseburger is topped 
+                <span className="more">more</span>
+            </section>
+            <section className="toppingsContent">
+                <h4 className="title">Toppings for you</h4>
+                
+            </section>
         </div>
     )
 }
